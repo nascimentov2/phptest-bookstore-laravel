628120e9d032f45b3c6f291804baaee09512967f
Edson Nascimento <enascimentosousa@gmail.com>
2023-06-23 03:22:41 -0300

    fixing migration and adding an exclusive database for testing in .env-local

---
+++ backend/tests/Feature/Api/V1/BookControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Api\V1;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookControllerTest extends TestCase
+{
+    public function test_index(): void
+    {
+        
+    }
+}
--- backend/tests/Feature/Auth/AuthenticationTest.php
+++ backend/tests/Feature/Auth/AuthenticationTest.php
@@ -10,24 +10,11 @@ class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_users_can_authenticate_using_the_login_screen(): void
-    {
-        $user = User::factory()->create();
-
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertNoContent();
-    }
-
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
         $user = User::factory()->create();
 
-        $this->post('/login', [
+        $this->post('/api/v1/login', [
             'email' => $user->email,
             'password' => 'wrong-password',
         ]);
