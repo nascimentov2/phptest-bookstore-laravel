0a1af69cb2873b8de41815e6dd813441a105e22a
Edson Nascimento <enascimentosousa@gmail.com>
2023-06-23 05:07:04 -0300

    API basic authentication working

--- backend/tests/Feature/Api/V1/BookControllerTest.php
+++ backend/tests/Feature/Api/V1/BookControllerTest.php
@@ -8,8 +8,58 @@ use Tests\TestCase;
 
 class BookControllerTest extends TestCase
 {
-    public function test_index(): void
+    /**
+     * Test if all BookController method require authentication
+     *
+     * @dataProvider endPointsDataProvider
+     * @param string $url
+     * @param string $method
+     *
+     * @return void
+     */
+    public function test_ensure_all_endpoints_must_be_authenticated(string $method, string $url): void
+    {
+        $request = $this->$method($url);
+
+        $request->assertStatus(200);
+
+        $request->assertJson(['message' => 'Unauthenticated.']);
+    }
+
+    public function test_index_full_list(): void
     {
         
+    }
+
+    public function test_create_new_book(): void
+    {
+
+    }
+
+    public function test_retrieve_single_book(): void
+    {
+
+    }
+
+    public function test_update_single_book(): void
+    {
+
+    }
+
+    public function test_delete_single_book(): void
+    {
+
+    }
+
+    public static function endPointsDataProvider(): array
+    {
+        return 
+        [
+            'get /api/v1/books'      => ['get', '/api/v1/books'],
+            'post /api/v1/books'     => ['post', '/api/v1/books'],
+            'get /api/v1/books/1'    => ['get', '/api/v1/books/1'],
+            'put /api/v1/books/1'    => ['put', '/api/v1/books/1'],
+            'delete /api/v1/books/1' => ['delete', '/api/v1/books/1'],
+        ];
     }
 }
